f785d695fd5d542708d69f44ae55e82f61690850
Mauricio Rivera <mauricio.rivera@gmail.com>
2010-09-13 12:47:25 -0400

    First commit. Has the default symfony 1.4.6 project structure, no changes made.

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/home/rivoot/Recursos/Frameworks/symfony-1.4.6/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
