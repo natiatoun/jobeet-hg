fa94e66bf86e59a0a6288b1dff44be20e23c02e3
Mauricio Rivera <mauricio.rivera@gmail.com>
2010-09-17 00:04:49 -0400

    End of day 5, mostly routing.

--- apps/frontend/modules/job/templates/indexSuccess.php
+++ apps/frontend/modules/job/templates/indexSuccess.php
@@ -2,10 +2,11 @@
 
 <div id="jobs">
     <table class="jobs">
-        <?php foreach($jobeet_jobs as $i => $job): ?>
-        <tr class="<?php echo fmod($i, 2)? 'even' : 'odd' ?>">
-            <td class="location"><?php echo $job->getLocation(); ?></td>
-            <td class="position"><a href="<?php echo url_for('job/show?id='. $job->getId()) ?>"><?php echo $job->getPosition(); ?></a></td>
+        <?php foreach ($jobeet_jobs as $i => $job): ?>
+            <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>">
+                <td class="location"><?php echo $job->getLocation(); ?></td>
+                <!--<td class="position"><a href="<?php echo url_for('job_show_user', $job) ?>"><?php echo $job->getPosition(); ?></a></td>-->
+                <td class="position"><?php echo link_to($job->getPosition(), 'job_show_user', $job) ?></td>
             <td class="company"><?php echo $job->getCompany(); ?></td>
         </tr>
         <?php endforeach ?>
