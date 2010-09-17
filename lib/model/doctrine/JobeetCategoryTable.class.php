ad67ee28694f2a0c7d59254be81da004c305cef7
Mauricio Rivera <mauricio.rivera@gmail.com>
2010-09-17 14:11:33 -0400

    End of day 6.

--- lib/model/doctrine/JobeetCategoryTable.class.php
+++ lib/model/doctrine/JobeetCategoryTable.class.php
@@ -1,11 +1,17 @@
 <?php
 
+class JobeetCategoryTable extends Doctrine_Table {
 
-class JobeetCategoryTable extends Doctrine_Table
-{
-    
-    public static function getInstance()
-    {
+    public static function getInstance() {
         return Doctrine_Core::getTable('JobeetCategory');
     }
+
+    public function getWithJobs() {
+        $q = $this->createQuery('c')
+                ->leftJoin('c.JobeetJobs j')
+                ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()));
+        
+        return $q->execute();
+    }
+
 }
